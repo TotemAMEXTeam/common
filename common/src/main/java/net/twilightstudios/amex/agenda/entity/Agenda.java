fbe27180eddc24f80ad0b98503c32f4aabb021d1
Sandra <sandraebro@gmail.com>
2015-10-11 16:45:07 +0200

    Entidad agenda

---
+++ common/src/main/java/net/twilightstudios/amex/agenda/entity/Agenda.java
@@ -0,0 +1,56 @@
+package net.twilightstudios.amex.agenda.entity;
+
+import java.util.List;
+
+import net.twilightstudios.amex.flight.entity.FlightStatus;
+import net.twilightstudios.amex.language.entity.LanguageExpression;
+import net.twilightstudios.amex.places.entity.Place;
+import net.twilightstudios.amex.weather.entity.Forecast;
+
+public class Agenda {
+	
+	private List<Place> restaurants;
+	private List<Place> activities;
+	private FlightStatus flightStatus;
+	private List<LanguageExpression> expressions;
+	private List<Forecast> weatherForecast;
+	private byte[] map;
+	
+	public List<Place> getRestaurants() {
+		return restaurants;
+	}
+	public void setRestaurants(List<Place> restaurants) {
+		this.restaurants = restaurants;
+	}
+	public List<Place> getActivities() {
+		return activities;
+	}
+	public void setActivities(List<Place> activities) {
+		this.activities = activities;
+	}
+	public FlightStatus getFlightStatus() {
+		return flightStatus;
+	}
+	public void setFlightStatus(FlightStatus flightStatus) {
+		this.flightStatus = flightStatus;
+	}
+	public List<LanguageExpression> getExpressions() {
+		return expressions;
+	}
+	public void setExpressions(List<LanguageExpression> expressions) {
+		this.expressions = expressions;
+	}
+	public byte[] getMap() {
+		return map;
+	}
+	public void setMap(byte[] map) {
+		this.map = map;
+	}
+	public List<Forecast> getWeatherForecast() {
+		return weatherForecast;
+	}
+	public void setWeatherForecast(List<Forecast> weatherForecast) {
+		this.weatherForecast = weatherForecast;
+	}
+	
+}
